e33a4150748449f78fde2e90bb307ea00e6736b0
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-09 16:09:57 +0200

    Update mastermind.py
    Wrote out rules

--- mastermind.py
+++ mastermind.py
@@ -1,3 +1,9 @@
+# Rules of Mastermind:
+# The 'codemaker' chooses four pieces from six colors, or blank.
+# The 'codebreaker' guesses the four colors.
+# The codemaker reports how many pieces are in the correct position,
+# and how many are a color that is present but not in the correct position.
+
 code = '1234'
 guess = input('Make your guess: ')
 if guess == code:
