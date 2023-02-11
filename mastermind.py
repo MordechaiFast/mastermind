58ba88a980a2ff308250fb8af5090edf9c042b3b
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-11 22:35:21 +0200

    Update mastermind.py
    Basic easy level annalysis

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,7 @@
 # Rules of Mastermind:
-# The 'codemaker' chooses four pieces from six colors, or blank.
+# The 'codemaker' chooses four pieces from six colors.
+# At easy level, repeat colors are not allowed.
+# At hard level, repeats and blanks are allowed.
 # The 'codebreaker' guesses the four colors.
 # The codemaker reports how many pieces are in the correct position,
 # and how many are a color that is present but not in the correct position.
@@ -9,4 +11,13 @@ guess = input('Make your guess: ')
 if guess == code:
     print('right')
 else:
-    print('wrong')
+    correct_position = {
+        guess_diget
+        for guess_diget, code_diget in zip(guess, code)
+        if guess_didget == code_diget
+    }
+    guess_colors = set(guess)
+    code_colors = set(code)
+    correct_colors = guess_colors & code_colors - correct_position
+    print(f'You have {len(correct_position)} in the right place,)
+    print(f'and {len(corect_colors)} others that are the right colors.')
