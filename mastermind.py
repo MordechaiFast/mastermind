32e5e1322cae51245c28b95304376659ec566981
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-12 22:32:57 +0200

    Multiple turns

--- mastermind.py
+++ mastermind.py
@@ -7,17 +7,20 @@
 # and how many are a color that is present but not in the correct position.
 '''
 
+num_of_guesses = 8
 code = '1234'
-guess = input('Make your guess: ')
-#sets might only work for easy level...
-correct_position = {
-    guess_diget
-    for guess_diget, code_diget in zip(guess, code)
-    if guess_diget == code_diget
-}
-correct_colors = set(guess) & set(code) - correct_position
-if len(correct_position) == 4:
-    print('Right')
-else:
-    print(f'You have {len(correct_position)} in the right place,')
-    print(f'and {len(correct_colors)} others that are the right colors.')
+for guess_num in range(1, num_of_guesses + 1):
+    guess = input(f'Guess #{guess_num}: ')
+    #sets might only work for easy level...
+    correct_position = {
+        guess_diget
+        for guess_diget, code_diget in zip(guess, code)
+        if guess_diget == code_diget
+    }
+    correct_colors = set(guess) & set(code) - correct_position
+    if len(correct_position) == 4:
+        print('Right')
+        break
+    else:
+        print(f'You have {len(correct_position)} in the right place,')
+        print(f'and {len(correct_colors)} others that are the right colors.')
