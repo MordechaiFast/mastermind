a312f2b3e2c4f08e90202bf99373767c5a5d8746
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-13 22:40:38 +0000

    crashed state

--- mastermind.py
+++ mastermind.py
@@ -3,22 +3,9 @@
 # At easy level, repeat colors are not allowed.
 # At hard level, repeats and blanks are allowed.
 # The 'codebreaker' guesses the four colors.
-# The codemaker reports how many pieces are in the correct position,
+# The 'codemaker' reports how many pieces are in the correct position,
 # and how many are a color that is present but not in the correct position.
 '''
 
 code = '1234'
-guess = input('Make your guess: ')
-if guess == code:
-    print('right')
-else:
-    correct_position = {
-        guess_diget
-        for guess_diget, code_diget in zip(guess, code)
-        if guess_diget == code_diget
-    }
-    guess_colors = set(guess)
-    code_colors = set(code)
-    correct_colors = guess_colors & code_colors - correct_position
-    print(f'You have {len(correct_position)} in the right place,')
-    print(f'and {len(correct_colors)} others that are the right colors.')
+guess = input('Make your guess: 'correct_position =     if guess_diget == code_dige    guess_digeguess_colors = set(guess    for guess_diget, code_diget in zip(guess, codecorrect_colors = guess_colors & code_colors - correct_positioprint(f'and {len(correct_colors)} others that are the right colors.'code_colors = set(codeprint(f'You have {len(correct_position)} in the right place,')
