868761a040528dc077b9781211149da62938dd73
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-14 00:30:14 +0200

    reworked scoring

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,5 @@
 '''Rules of Mastermind:
-# The 'codemaker' chooses four pieces from six colors.
+# The 'codemaker' chooses four 'pieces' from six 'colors'.
 # At easy level, repeat colors are not allowed.
 # At hard level, repeats and blanks are allowed.
 # The 'codebreaker' guesses the four colors.
@@ -7,28 +7,37 @@
 # and how many are a color that is present but not in the correct position.
 '''
 
-import random
+from random import randint
 
 
+level = 0
 num_of_guesses = 8
-code = []
-for _ in range(4):
-    # if level == 'easy':
-    while diget := random.randint(1, 6) in code:
-        pass
-    code.append(diget)
+if level != 0:
+    code = [randint(0 if level == 2 else 1, 6) for _ in '----']
+else:
+    code = []
+    for _ in '----':
+        while (diget := randint(1, 6)) in code:
+            pass
+        code.append(diget)
 for guess_num in range(1, num_of_guesses + 1):
     guess = input(f'Guess #{guess_num}: ')
-    #sets might only work for easy level...
-    correct_position = {
-        guess_diget
-        for guess_diget, code_diget in zip(guess, code)
-        if guess_diget == code_diget
-    }
-    correct_colors = set(guess) & set(code) - correct_position
-    if len(correct_position) == 4:
-        print('Right')
+    correct_digets, missplaced_digets = 0, 0
+    incorrect_guess_digets, unguessed_code_digets = [], []
+    for guess_diget, code_diget in zip(guess, code):
+        if guess_diget == code_diget:
+            correct_digets += 1
+        else:
+            incorrect_guess_digets.append(guess_diget)
+            unguessed_code_digets.append(code_diget)
+    for guess_diget in incorrect_guess_digets:
+        for code_diget in unguessed_code_digets:
+            if guess_diget == code_diget:
+                missplaced_digets += 1
+                del code_diget
+                break
+    print('*' * correct_digets + '-' * missplaced_digets)
+    if correct_digets == 4:
         break
-    else:
-        print(f'You have {len(correct_position)} in the right place,')
-        print(f'and {len(correct_colors)} others that are the right colors.')
+if [diget for diget in guess] != code:
+    print(diget for diget in code)
