1fa6c8a6fb6c65fa556221e32d24fe4aed0a2dcb
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-15 22:14:30 +0000

    Validation, documentation

--- mastermind.py
+++ mastermind.py
@@ -12,7 +12,7 @@ from random import randint
 
 def produce_code(level = 0) -> list:
     '''Generates a random four diget code, according to the Mastemind rules'''
-    
+
     if level != 0:
         code = [randint(0 if level == 2 else 1, 6) for _ in '----']
     else:
@@ -27,8 +27,8 @@ def score_guess(guess, code):
     '''Scores a guess against a given code.
     Allows the guess or the code to be given as a string or list of ints.
     Returns the number of places that are correct, and the number of places
-    that are present but not correct'''
-
+    that are present but not correct
+    '''
     correct_digits, missplaced_digits = 0, 0
     incorrect_guess_digits, unguessed_code_digits = [], []
     for guess_digit, code_digit in zip(guess, code):
@@ -48,14 +48,28 @@ def score_guess(guess, code):
 num_of_guesses = 8
 
 def main():
+    '''The flow function for the game.
+    Includes set-up and end game.
+    '''
+    # Set up
     code = produce_code()
+    # Turn loop
     for guess_num in range(1, num_of_guesses + 1):
-        guess = input(f'Guess #{guess_num}: ')
+        # Receive a guess
+        while ( # Validate guess format
+            not (guess := input(f'Guess #{guess_num}: ')).isdecimal()
+            or len(guess) != 4
+        ):
+            print('Enter four digits')
+        # Score the guess
         correct, missplaced = score_guess(guess, code)
+        # Report the score
         print('*' * correct + '-' * missplaced)
+        # Check for a win
         if correct == 4:
             return
+    # End game: reveal the code
     print(code[0], code[1], code[2], code[3], sep='')
 
-if __name__ == '--main__':
+if __name__ == '__main__':
     main()
